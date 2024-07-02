04af11bcd1b87b166b18f739977c8d50687e3e73
韩旭gogogo <js_bin@163.com>
2024-07-02 11:02:24 +0800

    feature: code add

---
+++ instanceof.js
@@ -0,0 +1,23 @@
+function instantof(left, right){
+    let proto = Object.getPrototypeOf(left)
+    let prototype = right.prototype
+    while(true){
+        if(proto === null) return false
+        if(proto === prototype) return true
+        proto = Object.getPrototypeOf(proto)
+    }
+}
+
+// test
+
+function A(){
+
+}
+
+const a = new A()
+
+console.log(instantof(a, A))
+
+console.log(instantof(a, Function))
+
+console.log(instantof(a, Object))
---
+++ promiseAll.js
@@ -0,0 +1,52 @@
+function promiseAll(promises) {
+    return new Promise((resolve, reject) => {
+        if(!Array.isArray(promises)) {
+            throw new TypeError('promises must be a array!')
+        }
+
+        let result = []
+        let successCount = 0
+        for(let i = 0 ; i<promises.length; i++){
+            Promise.resolve(promises[i]).then(rs => {
+                result[i] = rs
+                successCount++
+                if(successCount === promises.length){
+                    resolve(result)
+                }
+            },error => {
+                reject(error)
+            })
+        }
+    })
+}
+
+// test
+
+let p1 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve(1)
+    }, 1000)
+})
+let p2 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve(2)
+    }, 2000)
+})
+let p3 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve(3)
+    }, 3000)
+})
+
+let p4 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        reject(4)
+    }, 4000)
+})
+promiseAll([p3, p1, p2]).then(res => {
+    console.log(res) // [3, 1, 2]
+})
+
+promiseAll([p4, p1, p2]).catch(res => {
+    console.error(res) // error
+})

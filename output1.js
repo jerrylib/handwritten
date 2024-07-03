9c22a02a863e11193bfa6a78d14b3d5bd9a6d35b
韩旭gogogo <js_bin@163.com>
2024-07-03 19:18:10 +0800

    feat: code add

---
+++ output1.js
@@ -0,0 +1,20 @@
+console.log('1')
+setTimeout(() => {
+    console.log('2');
+}, 0)
+requestAnimationFrame(() => {
+    console.log('3') 
+})
+requestIdleCallback(() => {
+    console.log('4')
+})
+new Promise(resolve => {
+    console.log('5');
+}).then(value => {
+    console.log(value);
+});
+async function a() {
+    console.log(await '7');
+}
+a()
+console.log('8')
